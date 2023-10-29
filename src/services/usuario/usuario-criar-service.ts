b52a7ac18c7faabba161632b32c4e3c9db8a865a
JeanOmeg <jeanomeg3@gmail.com>
2023-10-29 15:28:44 -0300

    ajuste no eslint

--- src/services/usuario/usuario-criar-service.ts
+++ src/services/usuario/usuario-criar-service.ts
@@ -1,6 +1,6 @@
 import { ICadastroUsuario } from '@interfaces/usuario/ICadastroUsuario'
 import { CadastroUsuarioQuery } from '@models/usuario/cadastro-usuario-model'
 
-export async function criarUsuarioService (usuario_query: CadastroUsuarioQuery, dados_criação: ICadastroUsuario): Promise<ICadastroUsuario>{
+export async function criarUsuarioService (usuario_query: CadastroUsuarioQuery, dados_criação: ICadastroUsuario): Promise<ICadastroUsuario> {
   return await usuario_query.criarUsuario(dados_criação)
 }
