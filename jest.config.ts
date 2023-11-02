817f8674c3286b4fc67806d9dd07e84061993801
JeanOmeg <jeanomeg3@gmail.com>
2023-11-02 02:09:43 -0300

    ajuste no jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,32 +1,32 @@
 import type {Config} from '@jest/types'
-// Sync object
 const config: Config.InitialOptions = {
   verbose: false,
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   collectCoverage: false, // true para gerar o relatório de cobertura
-  coverageDirectory: 'tests',
+  coverageDirectory: 'src/tests',
   coveragePathIgnorePatterns: [
-    '<rootDir>/src/enum',
-    '<rootDir>/src/interface',
-    '<rootDir>/src/middleware',
-    '<rootDir>/src/notificacao',
-    '<rootDir>/src/schema'
+    '<rootDir>/src/enums',
+    '<rootDir>/src/interfaces',
+    '<rootDir>/src/middlewares',
+    '<rootDir>/src/schemas',
+    '<rootDir>/src/models'
   ],
-  collectCoverageFrom: ['src/**', '!src/interfaces/**', '!src/enums/**'],
+  collectCoverageFrom: ['src/**', '!src/interfaces/**', '!src/enums/**', '!src/tests/**'],
   testPathIgnorePatterns : [
     '/node_modules/'
   ],
   moduleNameMapper: {
-    '@routes/(.*)$': '<rootDir>/src/routes/$1',
-    '@models/(.*)$': '<rootDir>/database/models/$1',
-    '@services/(.*)$': '<rootDir>/src/services/$1',
-    '@tests/(.*)$': '<rootDir>/tests/$1',
-    '@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '@controllers/(.*)$': '<rootDir>/src/controllers/$1',
     '@enums/(.*)$': '<rootDir>/src/enums/$1',
     '@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
-    '@schemas/(.*)$': '<rootDir>/src/schemas/$1'
+    '@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '@models/(.*)$': '<rootDir>/src/models/$1',
+    '@routes/(.*)$': '<rootDir>/src/routes/$1',
+    '@schemas/(.*)$': '<rootDir>/src/schemas/$1',
+    '@services/(.*)$': '<rootDir>/src/services/$1',
+    '@tests/(.*)$': '<rootDir>/tests/$1'
   }
 }
 
