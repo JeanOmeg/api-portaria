f8c27eb97be738d839568bf2ad84169c53029ac5
JeanOmeg <jeanomeg3@gmail.com>
2023-11-04 15:14:48 -0300

    preparação dos seeders iniciais

--- database/seeder/20231104170047-atualizar-tabela-colaborador-adicionar-administrador.js
+++ database/seeder/20231104170047-atualizar-tabela-colaborador-adicionar-administrador.js
@@ -5,29 +5,29 @@ const tabela = 'colaborador'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
-    const data = [
-      {
-        role: 'Controlador',
-        data_criacao: new Date()
-      },
-      {
-        role: 'Condomino',
-        data_criacao: new Date()
-      },
-      {
-        role: 'Sindico',
-        data_criacao: new Date()
-      },
-      {
-        role: 'Administrador',
-        data_criacao: new Date()
-      }
-    ]
+    // const data = [
+    //   {
+    //     role: 'Controlador',
+    //     data_criacao: new Date()
+    //   },
+    //   {
+    //     role: 'Condomino',
+    //     data_criacao: new Date()
+    //   },
+    //   {
+    //     role: 'Sindico',
+    //     data_criacao: new Date()
+    //   },
+    //   {
+    //     role: 'Administrador',
+    //     data_criacao: new Date()
+    //   }
+    // ]
 
-    await queryInterface.bulkInsert(tabela, data, {})
+    // await queryInterface.bulkInsert(tabela, data, {})
   },
 
   async down (queryInterface) {
-    await queryInterface.bulkDelete(tabela, null, {})
+    // await queryInterface.bulkDelete(tabela, null, {})
   }
 }
